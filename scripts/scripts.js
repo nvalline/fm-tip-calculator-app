73ba17c828f6b7b84c47cdcbd17b46e10cc4f90f
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-06-10 15:21:35 -0600

    added calculations & error toasts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,13 @@
+const billError = document.getElementById('billError');
 const billTotalInput = document.getElementById('bill');
+const countError = document.getElementById('countError');
 const countInput = document.getElementById('count');
 const customTipButton = document.getElementById('customTip');
+const formInputs = document.querySelectorAll('input');
 const radioButtons = document.getElementsByName('tipAmount');
 const resetButton = document.getElementById('resetButton');
-const formInputs = document.querySelectorAll('input');
+const tipPerPersonEl = document.getElementById('tipPerPerson');
+const totalPerPersonEl = document.getElementById('totalPerPerson');
 
 let amounts = {
 	totalBill: '',
@@ -19,6 +23,61 @@ const removeCheckedStatus = () => {
 };
 
 customTipButton.addEventListener('focus', () => removeCheckedStatus());
+
+// Calculate Tip Amounts
+const calculateTipAmounts = (amounts) => {
+	const bill = parseFloat(amounts.totalBill);
+	const tipPercentage = parseFloat(amounts.tipPercentage);
+	const count = parseInt(amounts.numberOfPeople);
+
+	let tipPerPerson = ((bill * tipPercentage) / count).toFixed(2);
+	let totalPerPerson = (bill / count + parseFloat(tipPerPerson)).toFixed(2);
+
+	tipPerPersonEl.innerHTML = tipPerPerson;
+	totalPerPersonEl.innerHTML = totalPerPerson;
+};
+
+// Display Errors
+const displayBillError = () => {
+	billError.classList = 'errorText';
+	billTotalInput.style.outline = '2px solid var(--clr-alert-red)';
+};
+
+const displayCountError = () => {
+	countError.classList = 'errorText';
+	countInput.style.outline = '2px solid var(--clr-alert-red)';
+};
+
+// Hide Errors
+const hideBillError = () => {
+	billError.classList = 'errorText hidden';
+	billTotalInput.style.outline = '2px solid var(--clr-primary-cyan)';
+};
+
+const hideCountError = () => {
+	countError.classList = 'errorText hidden';
+	countInput.style.outline = '2px solid var(--clr-primary-cyan)';
+};
+
+// Validate Form Amounts
+const validateAmounts = (amounts) => {
+	if (amounts.totalBill <= 0) {
+		displayBillError();
+	} else {
+		hideBillError();
+	}
+
+	if (
+		amounts.totalBill > 0 &&
+		(amounts.numberOfPeople <= 0 || !amounts.numberOfPeople)
+	) {
+		displayCountError();
+		return;
+	} else {
+		hideCountError();
+		calculateTipAmounts(amounts);
+	}
+};
 
 // Get input values
 Array.prototype.forEach.call(formInputs, (input) => {
@@ -40,9 +99,8 @@ Array.prototype.forEach.call(formInputs, (input) => {
 				0;
 		}
 
-		console.log('AMOUNTS', amounts);
-
 		setResetBtnDisabled(amounts);
+		validateAmounts(amounts);
 	});
 });
 
